4f85283cc96fc13f3131e803390733ccb6e2b702
Feka7 <feka7@hotmail.it>
2024-03-29 21:21:59 +0100

    add create stream function

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,61 +1,296 @@
 "use client";
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { ChangeEvent, useState } from "react";
+import toast from "react-hot-toast";
+import { Address } from "viem";
+import { useAccount, useWriteContract } from "wagmi";
+import { namehash } from "viem/ens";
 
 export default function Page() {
+  const { writeContractAsync } = useWriteContract();
+  const { address } = useAccount()
+  const [ensDomain, setEnsDomain] = useState("");
+  const [amount, setAmount] = useState("");
+  const [token, setToken] = useState("");
+  const [cancelable, setCancelable] = useState("");
+  const [transferable, setTransferable] = useState("");
+  const [start, setStart] = useState("");
+  const [duration, setDuration] = useState("");
+
+  async function approve() {
+    await writeContractAsync({
+      abi: [
+        {
+          constant: false,
+          inputs: [
+            {
+              name: "_spender",
+              type: "address",
+            },
+            {
+              name: "_value",
+              type: "uint256",
+            },
+          ],
+          name: "approve",
+          outputs: [
+            {
+              name: "",
+              type: "bool",
+            },
+          ],
+          payable: false,
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ],
+      address: token as Address,
+      functionName: "approve",
+      args: [
+        process.env.NEXT_PUBLIC_RAPIDO_SMART_CONTRACT,
+        Number(amount) * 1e18,
+      ],
+    });
+
+    toast.success("Token approved succesfully!");
+  }
+
+  async function createStream() {
+    await writeContractAsync({
+      abi: [
+        {
+          inputs: [
+            {
+              components: [
+                {
+                  internalType: "address",
+                  name: "sender",
+                  type: "address",
+                },
+                {
+                  internalType: "address",
+                  name: "recipient",
+                  type: "address",
+                },
+                {
+                  internalType: "uint128",
+                  name: "totalAmount",
+                  type: "uint128",
+                },
+                {
+                  internalType: "contract IERC20",
+                  name: "asset",
+                  type: "address",
+                },
+                {
+                  internalType: "bool",
+                  name: "cancelable",
+                  type: "bool",
+                },
+                {
+                  internalType: "bool",
+                  name: "transferable",
+                  type: "bool",
+                },
+                {
+                  components: [
+                    {
+                      internalType: "uint40",
+                      name: "cliff",
+                      type: "uint40",
+                    },
+                    {
+                      internalType: "uint40",
+                      name: "total",
+                      type: "uint40",
+                    },
+                  ],
+                  internalType: "struct LockupLinear.Durations",
+                  name: "durations",
+                  type: "tuple",
+                },
+                {
+                  components: [
+                    {
+                      internalType: "address",
+                      name: "account",
+                      type: "address",
+                    },
+                    {
+                      internalType: "UD60x18",
+                      name: "fee",
+                      type: "uint256",
+                    },
+                  ],
+                  internalType: "struct Broker",
+                  name: "broker",
+                  type: "tuple",
+                },
+              ],
+              internalType: "struct LockupLinear.CreateWithDurations",
+              name: "params",
+              type: "tuple",
+            },
+            {
+              internalType: "bytes32",
+              name: "node",
+              type: "bytes32",
+            },
+          ],
+          name: "createWithDurations",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "streamId",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ],
+      address: process.env.NEXT_PUBLIC_RAPIDO_SMART_CONTRACT as Address,
+      functionName: "createWithDurations",
+      args: [
+        [
+          address,
+          process.env.NEXT_PUBLIC_RAPIDO_SMART_CONTRACT as Address,
+          amount,
+          token,
+          cancelable === "True" ? true : false,
+          transferable === "True" ? true : false,
+          [0, 3600],
+          ["0x0000000000000000000000000000000000000000", "0"],
+        ],
+        namehash(ensDomain),
+      ],
+    });
+  }
+
+  const handleEnsDomainChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEnsDomain(event.target.value);
+  };
+
+  const handleAmountChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setAmount(event.target.value);
+  };
+
+  const handleTokenChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setToken(event.target.value);
+  };
+
+  const handleCancelableChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setCancelable(event.target.value);
+  };
+
+  const handleTransferableChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setTransferable(event.target.value);
+  };
+
+  const handleStartChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setStart(event.target.value);
+  };
+
+  const handleDurationChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setDuration(event.target.value);
+  };
+
   return (
-    <main className="min-h-screen flex flex-col justify-items-center items-center">
+    <main className="min-screen flex flex-col justify-items-center items-center">
       <div className="p-4">
         <ConnectButton />
       </div>
-      <div className="card shadow card-bordered">
+      <div className=" shadow card-bordered">
         <div className="card-body">
-          <label className="input input-bordered flex items-center gap-2">
+          <h3 className="card-title">Create stream</h3>
+          <label className="input input-bordered flex items items-center gap-2">
             Ens domain
-            <input type="text" className="grow" placeholder="example.eth" />
+            <input
+              type="text"
+              className="grow"
+              value={ensDomain}
+              onChange={handleEnsDomainChange}
+            />
           </label>
           <label className="input input-bordered flex items-center gap-2">
             Amount
-            <input type="text" className="grow" placeholder="" />
+            <input
+              type="text"
+              className="grow"
+              placeholder=""
+              value={amount}
+              onChange={handleAmountChange}
+            />
           </label>
-          <select className="select select-bordered w-full max-w-xs">
-            <option disabled selected>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            value={token}
+            onChange={handleTokenChange}
+          >
+            <option disabled value="">
               Token
             </option>
-            <option>DAI</option>
-            <option>WBTC</option>
+            <option value="0x6b175474e89094c44da98b954eedeac495271d0f">
+              DAI
+            </option>
+            <option value="0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3">
+              MIM
+            </option>
           </select>
-          <select className="select select-bordered w-full max-w-xs">
-            <option disabled selected>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            value={cancelable}
+            onChange={handleCancelableChange}
+          >
+            <option disabled value="">
               Cancelable
             </option>
-            <option>True</option>
-            <option>False</option>
+            <option value="True">True</option>
+            <option value="False">False</option>
           </select>
-          <select className="select select-bordered w-full max-w-xs">
-            <option disabled selected>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            value={transferable}
+            onChange={handleTransferableChange}
+          >
+            <option disabled value="">
               Transferable
             </option>
-            <option>True</option>
-            <option>False</option>
+            <option value="True">True</option>
+            <option value="False">False</option>
           </select>
-          <select className="select select-bordered w-full max-w-xs">
-            <option disabled selected>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            value={start}
+            onChange={handleStartChange}
+          >
+            <option disabled value="">
               Start
             </option>
-            <option>Now</option>
-            <option>1 day</option>
-            <option>1 week</option>
+            <option value="0">Now</option>
+            <option value="3600">1 hour</option>
+            <option value="86400">1 day</option>
           </select>
-          <select className="select select-bordered w-full max-w-xs">
-            <option disabled selected>
+          <select
+            className="select select-bordered w-full max-w-xs"
+            value={duration}
+            onChange={handleDurationChange}
+          >
+            <option disabled value="">
               Duration
             </option>
-            <option>1 hpur</option>
-            <option>1 day</option>
-            <option>1 week</option>
+            <option value="3600">1 hour</option>
+            <option value="86400">1 day</option>
+            <option value="604800">1 week</option>
           </select>
         </div>
+        <button className="btn" onClick={approve}>
+          Approve
+        </button>
+        <button className="btn" onClick={createStream}>
+          Create
+        </button>
       </div>
     </main>
   );
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,30 +1,40 @@
 "use client";
 import { ReactNode } from "react";
 import { Toaster } from "react-hot-toast";
-import '@rainbow-me/rainbowkit/styles.css';
+import "@rainbow-me/rainbowkit/styles.css";
 
-import {
-  getDefaultConfig,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
 import {
   mainnet,
   polygon,
   optimism,
   arbitrum,
   base,
-  sepolia
-} from 'wagmi/chains';
-import {
-  QueryClientProvider,
-  QueryClient,
-} from "@tanstack/react-query";
+  sepolia,
+  Chain,
+} from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const tenderly_fork_chain = {
+  id: parseInt(process.env.NEXT_PUBLIC_TENDERLY_CHAIN_ID as string),
+  name: "Tenderly",
+  nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
+  rpcUrls: {
+    default: { http: [process.env.NEXT_PUBLIC_TENDERLY_FORK_URL as string] },
+  },
+  testnet: true,
+} as const satisfies Chain;
 
 const config = getDefaultConfig({
-  appName: 'Rapido',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+  appName: "Rapido",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [
+    {
+      ...tenderly_fork_chain,
+      iconBackground: "#6e56cf",
+    },
+  ],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
@@ -37,13 +47,11 @@ const queryClient = new QueryClient();
 export default function Providers({ children }: Props) {
   return (
     <>
-        <WagmiProvider config={config} >
-          <QueryClientProvider client={queryClient}>
-          <RainbowKitProvider>
-              {children}
-              </RainbowKitProvider>
-          </QueryClientProvider>
-        </WagmiProvider>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>{children}</RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
       <Toaster
         toastOptions={{
           style: {
