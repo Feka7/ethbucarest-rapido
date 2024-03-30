6f4db360191e55b6eb74b35a1c2a1bbc686a8cdb
Leoparme <leoparme@live.it>
2024-03-30 02:10:19 +0100

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,15 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'custom-purple': '0 -5px 15px -0px rgba(128, 90, 213, 1), 0 10px 15px -5px rgba(128, 90, 213, 1)',
+      },
+    },
+  },
   plugins: [require("daisyui")],
 };
+
 export default config;
+
