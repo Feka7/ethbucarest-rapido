140026bf62d0d24ba301aefd678fabf4a5bbeac1
maromandarino <maromandarino@gmail.com>
2024-03-30 03:24:17 +0100

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,8 +35,7 @@ export default function Page() {
             <span className="text-[#79DFFF]">O</span>perations
           </h1>
           <p className="mb-6 leading-normal text-xl">
-            Stream payments directly to departments with ENS domains and
-            subdomains
+          We redefine how DAOs handle payroll, making it as dynamic and flexible as the teams that drive them
           </p>
           <Link href="/app" className="enter-app-btn">Enter App</Link>
         </div>
@@ -47,94 +46,6 @@ export default function Page() {
             width={400}
             height={200}
             className="float"
-          />
-        </div>
-      </div>
-      <div className="banner overflow-hidden flex items-center py-2.5 w-full mx-auto max-w-none mb-24 mt-11">
-        <div className="animate-slide infinite-scroll flex object-contain">
-          <Image
-            src="/logobanner1.png"
-            alt="logobanner1"
-            width={180}
-            height={80}
-            className="mx-4"
-          />
-          <Image
-            src="/logobanner2.png"
-            alt="logobanner2"
-            width={180}
-            height={80}
-            className="mx-4"
-          />
-          <Image
-            src="/logobanner3.png"
-            alt="logobanner3"
-            width={180}
-            height={80}
-            className="mx-4"
-          />
-          <Image
-            src="/logobanner4.png"
-            alt="logobanner4"
-            width={180}
-            height={80}
-            className="mx-4"
-          />
-          <Image
-            src="/logobanner1.png"
-            alt="logobanner1"
-            width={180}
-            height={80}
-            className="mx-4"
-          />
-          <Image
-            src="/logobanner2.png"
-            alt="logobanner2"
-            width={180}
-            height={80}
-            className="mx-4"
-          />
-          <Image
-            src="/logobanner3.png"
-            alt="logobanner3"
-            width={180}
-            height={80}
-            className="mx-4"
-          />
-          <Image
-            src="/logobanner4.png"
-            alt="logobanner4"
-            width={180}
-            height={80}
-            className="mx-4"
-          />
-          <Image
-            src="/logobanner1.png"
-            alt="logobanner1"
-            width={180}
-            height={80}
-            className="mx-4"
-          />
-          <Image
-            src="/logobanner2.png"
-            alt="logobanner2"
-            width={180}
-            height={80}
-            className="mx-4"
-          />
-          <Image
-            src="/logobanner3.png"
-            alt="logobanner3"
-            width={180}
-            height={80}
-            className="mx-4"
-          />
-          <Image
-            src="/logobanner4.png"
-            alt="logobanner4"
-            width={180}
-            height={80}
-            className="mx-4"
           />
         </div>
       </div>
@@ -152,12 +63,10 @@ export default function Page() {
             alt="map"
           />
           <p className="mb-[10px] text-4xl font-bold">
-            Seamless setup for DAOs
+          Seamless setup for DAOs
           </p>
           <p className="max-w-md font-semibold mt-4">
-            Begin by mapping your DAO&apos;s departments to specific ENS subdomains,
-            such as product.dao.eth or marketing.dao.eth. This one-time setup
-            aligns your organizational structure with your payment streams.
+          mapping DAO’s teams to specific ENS subdomains - this one-time setup aligns the DAO organizational structure with payment streams
           </p>
         </div>
         <div className="flex-1 flex justify-center items-center md:p-8">
@@ -171,12 +80,10 @@ export default function Page() {
         <div className="flex-1 flex flex-col justify-center items-start text-left md:p-8">
           {/* INSERIRE gif */}
           <p className="mb-[10px] text-4xl font-bold">
-            Department-specific streams
+          Stream payments effortlessly with Sablier
           </p>
           <p className="max-w-md font-semibold mt-4">
-            Use Sablier to direct funds to each department&apos;s ENS subdomain.Set
-            up streams based on your payroll schedule, whether it&apos;s for
-            short-term projects or ongoing compensation.
+          employees or collaborators can withdraw their accumulated funds at any point from their specific subdomain
           </p>
         </div>
       </div>
@@ -184,13 +91,10 @@ export default function Page() {
         <div className="flex-1 flex flex-col justify-center items-start text-left md:p-8">
           {/* INSERIRE gif */}
           <p className="mb-[10px] text-4xl font-bold">
-            Dynamic ENS integration
+          Dynamic ENS integration 
           </p>
           <p className="max-w-md font-semibold mt-4">
-            We leverage ENS to make payments easy to manage and remember.Instead
-            of dealing with complex wallet addresses, funds are streamed to
-            recognizable and memorable subdomains aligned with your DAO&apos;s
-            structure.
+          ENS makes payments easy to manage and remember - funds are streamed to recognizable and memorable subdomains aligned with the DAO's structure
           </p>
         </div>
         <div className="flex-1 flex justify-center items-center md:p-8">
@@ -205,93 +109,57 @@ export default function Page() {
           {/* INSERIRE gif */}
           <p className="mb-[10px] text-4xl font-bold">Ownership flexibility</p>
           <p className="max-w-md font-semibold mt-4">
-            As roles within your DAO evolve, easily redirect payment streams to
-            new recipients under the same departmental subdomain, without any
-            interruption. This flexibility ensures that your team continues to
-            receive their earnings in real-time, even as the organization grows
-            and changes.
+          as roles within a DAO evolve, payment streams can be easily redirected to new recipients under the same subdomain, without any interruption
           </p>
         </div>
       </div>
-      <div className="text-white p-12 flex flex-col md:flex-row">
-        <div className="flex-1 flex flex-col justify-center items-start text-left md:p-8">
-          {/* INSERIRE gif */}
-          <p className="mb-[10px] text-4xl font-bold">Withdraw anytime</p>
-          <p className="max-w-md font-semibold mt-4">
-            Employees or collaborators can withdraw their accumulated funds at
-            any point from their department&apos;s subdomain. This real-time access
-            to earnings empowers them with financial flexibility and security.
-          </p>
-        </div>
-        <div className="flex-1 flex justify-center items-center md:p-8">
-          <Image src="/daiwith.png" width={300} height={300} alt="work5" />
-        </div>
-      </div>
+
+
+
+
       <div className="mt-[50px] text-center" id="company">
         <p className="text-4xl font-bold gradient-text">
-          Company/DAO perspective
+          For DAOs
         </p>
         <div className="divider"></div>
       </div>
       <div className="text-white grid grid-cols-3 gap-4 p-4 mb-[120px]">
         <div className="card w-96 glass hover:scale-105 transition-transform duration-300">
           <div className="card-body">
-            <h2 className="card-title">Flexibility and efficiency: </h2>
-            <p>optimize team compensation across all DAO verticals.</p>
+            <h2 className="card-title">Ensures high-quality deliverables through continuous evaluation</h2>
           </div>
         </div>
         <div className="card w-96 glass hover:scale-105 transition-transform duration-300">
           <div className="card-body">
-            <h2 className="card-title">Simplify administrative tasks:</h2>
-            <p>
-              reduce the burden with payments tied to ENS domains, not
-              individual NFT streams.
-            </p>
+            <h2 className="card-title">Makes interaction easy with a user-friendly interface</h2>
           </div>
         </div>
         <div className="card w-96 glass hover:scale-105 transition-transform duration-300">
           <div className="card-body">
-            <h2 className="card-title">Adaptable ownership:</h2>
-            <p>
-              easily redirect streams as roles within your DAO change, ensuring
-              uninterrupted payroll continuity.
-            </p>
+            <h2 className="card-title">Cuts down boring and errore prone administrative work</h2>
           </div>
         </div>
       </div>
       <div className="mt-[50px] text-center" id="recipient">
         <p className="text-4xl font-bold gradient-text">
-          Recipient (Contributor) perspective
+          For Contributors
         </p>
         <div className="divider"></div>
       </div>
       <div className="text-white grid grid-cols-3 gap-4 p-4">
         <div className="card w-96 glass hover:scale-105 transition-transform duration-300">
           <div className="card-body">
-            <h2 className="card-title">Real-time earning: </h2>
-            <p>
-              payments now move at a pace that matches the pace of web3
-              projects!
-            </p>
+            <h2 className="card-title">Streamlines the funding process, allowing for quicker project starts</h2>
           </div>
         </div>
         <div className="card w-96 glass hover:scale-105 transition-transform duration-300">
           <div className="card-body">
-            <h2 className="card-title">Personalized payment streams:</h2>
-            <p>
-              withdraw your salary directly to department-specific ENS
-              subdomains.
-            </p>
+            <h2 className="card-title">Provides a stable stream of funding, ensuring financial security</h2>
           </div>
         </div>
         <div className="card w-96 glass hover:scale-105 transition-transform duration-300">
           <div className="card-body">
-            <h2 className="card-title">Adaptive compensation:</h2>
-            <p>
-              as your role in the DAO evolves, your payment stream adjusts
-              seamlessly, ensuring you&apos;re compensated for your current
-              contributions without interruption.
-            </p>
+            <h2 className="card-title">Opens up funding opportunities for innovative small-scale initiatives</h2>
           </div>
         </div>
       </div>
